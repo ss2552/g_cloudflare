e6e7d282bed06acbcce840bcc5e3133d249b0203
ss2552 <85072753+ss2552@users.noreply.github.com>
2024-11-29 02:22:23 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,15 +17,5 @@ export default{
         	console.log(await request.text())
 		}
 		return new Response("{}", {headers: {'content-type': 'application/json;charset=UTF-8'}})
-    },
+    }
 }
-
-/*
-fetch(`https://dash.cloudflare.com/api/v4/accounts/${account_id}/workers/observability/telemetry/query`, {
-  "headers": {
-    "content-type": "application/json"
-  },
-  "body": "{\"timeframe\":{\"to\":1732354780864,\"from\":1732095580864},\"view\":\"events\",\"limit\":100,\"dry\":true,\"queryId\":\"workers-logs\",\"parameters\":{\"datasets\":[\"cloudflare-workers\"],\"filters\":[{\"key\":\"$baselime.service\",\"type\":\"string\",\"value\":\"g\",\"operation\":\"=\"}]}}",
-  "method": "POST"
-});
-*/
