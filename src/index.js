d72d016e6c34900ebe52defcff4f6ec5e1382cd6
ss2552 <85072753+ss2552@users.noreply.github.com>
2024-11-29 19:29:10 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
 
 export default{
     async fetch(request, env, ctx){
-		const pn = new URL(request.url).pathname || "/"
+	const pn = new URL(request.url).pathname || "/"
 
+        if(request.method == "GET"){
+        	return new Response("Not Found.", {status: 404})
+        }else{
+        	console.log(await request.text())
 		//接続をウェブフックを使いディスコードで通知する
 		try{
 			await fetch(env.WH_URL, {headers: {"content-type": "application/json"}, body: JSON.stringify({content: `パスネーム ${pn}\nメソッド ${request.method}`}), method: "POST"})
@@ -11,11 +15,7 @@ export default{
 			console.log(e)
 		}
 		//ゲットならステータス404を返す
-        if(request.method == "GET"){
-        	return new Response("Not Found.", {status: 404})
-        }else{
-        	console.log(await request.text())
-		}
-		return new Response("{}", {headers: {'content-type': 'application/json;charset=UTF-8'}})
+	}
+	return new Response("{}", {headers: {'content-type': 'application/json;charset=UTF-8'}})
     }
 }
